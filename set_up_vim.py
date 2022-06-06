e096dbb2c3c0851fd8338aa69eff13b702420f8c
Konstantinos Drosos <kostas.drosos@icloud.com>
2022-06-06 20:46:04 +0300

    Added functionality

--- set_up_vim.py
+++ set_up_vim.py
@@ -10,8 +10,49 @@ Brew is needed to use this script.
 __author__ = 'Konstantinos Drossos'
 __docformat__ = 'reStructuredText'
 
+from argparse import ArgumentParser
 from pathlib import Path
 from subprocess import run, PIPE
+
+
+def get_argument_parser() -> ArgumentParser:
+    arg_parser = ArgumentParser()
+
+    args = [
+        [
+            ['--install-fonts'],
+            {
+                'action': 'store_true',
+                'help': 'Install available NerdFonts from Homebrew',
+            },
+        ],
+        [
+            ['--instal-vin'],
+            {
+                'action': 'store_true',
+                'help': 'Install Vim from Homebrew',
+            },
+        ],
+        [
+            ['--install-plug'],
+            {
+                'action': 'store_true',
+                'help': 'Install Vim Plug',
+            },
+        ],
+        [
+            ['install-vim-plugins'],
+            {
+                'action': 'store_true',
+                'help': 'Install Vim plugins',
+            },
+        ],
+    ]
+
+    for arg in args:
+        arg_parser.add_argument(*arg[0], **arg[1])
+
+    return arg_parser
 
 
 def arrange_files() -> None:
@@ -36,6 +77,10 @@ def install_fonts() -> None:
         run(f'brew install {font_name}', shell=True)
 
 
+def install_vim() -> None:
+    run('brew install vim', shell=True)
+
+
 def download_plug() -> None:
     run(
         'curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
@@ -47,7 +92,20 @@ def install_plugins() -> None:
 
 
 def main():
-    install_fonts()
+    arg_parser = get_argument_parser()
+    args = arg_parser.parse_args()
+
+    if args.install_fonts:
+        install_fonts()
+    if args.install_vim:
+        install_vim()
+    if args.install_plug:
+        download_plug()
+
+    arrange_files()
+
+    if args.install_vim_plugins:
+        install_plugins()
 
 
 if __name__ == '__main__':
